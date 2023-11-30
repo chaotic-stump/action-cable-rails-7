be8ca1580eec2e0ab95e20b9f30ef8fc83bdd6fe
Sean Parker <seanrichardparker@gmail.com>
2023-11-29 20:29:13 -0500

    troubleshoot routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :rooms, only: [:index, :show]
+  resources :rooms, only: %i[index, show]
   get 'rooms/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
